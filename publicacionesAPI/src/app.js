6b0ec64c387a10ebb6a01a26f9f2489fed397176
Bar Sinson <dzvaldiviesod@gmail.com>
2023-10-25 09:42:27 -0500

    api def

---
+++ publicacionesAPI/src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const routes = require('src/routes/index');
+app.use('/publicacionesAPI', routes);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('Server is running on port ' + port);
+});
+
