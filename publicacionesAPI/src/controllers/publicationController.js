6937cd7e6adbc67750ce79f1b399e8f48877cb7e
felixasela <felixga1324@gmail.com>
2023-10-25 15:15:16 -0500

    API publish

--- publicacionesAPI/src/controllers/publicationController.js
+++ publicacionesAPI/src/controllers/publicationController.js
@@ -11,3 +11,26 @@ exports.getPublish = async (req, res) => {
         });
     };
 };
+
+exports.createPublish = async (req, res) => {
+    try {
+        const { imgRoute, videoRoute, textPublication, userName } = req.body;
+
+        const [result] = await conn.execute(
+            'INSERT INTO publicacion (imgRoute, videoRoute, textPublication, userName) VALUES (?, ?, ?, ?)',
+            [imgRoute, videoRoute, textPublication, userName]
+        );
+
+        const insertedId = result.insertId;
+
+        return res.status(201).json({
+            message: 'Publicación creada exitosamente',
+            insertedId: insertedId
+        });
+    } catch (error) {
+        console.error('Error al crear la publicación:', error);
+        return res.status(500).json({
+            message: 'Algo ha salido mal'
+        });
+    }
+};
